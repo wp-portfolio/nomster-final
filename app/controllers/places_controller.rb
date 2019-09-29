4ef97453080f12de9fec547307e63b3323594a1b
wp-portfolio <wpanowyk1@gmail.com>
2019-09-29 08:41:59 +0000

    hooked up update and edit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,6 +22,11 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
 
   private
 
