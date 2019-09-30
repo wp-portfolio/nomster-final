3efc006eeb13104bc99a23ca94132c4d14a85f42
wp-portfolio <wpanowyk1@gmail.com>
2019-09-30 00:08:57 +0000

    partial - update later

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_231825) do
+ActiveRecord::Schema.define(version: 2019_09_29_235813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_09_29_231825) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["place_id"], name: "index_photos_on_place_id"
     t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
   end
